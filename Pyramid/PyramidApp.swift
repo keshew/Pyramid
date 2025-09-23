94f02824fc8d8825903f472e0d0a29775bf8236f
keshew <artyom.korotckov2017@yandex.ru>
2025-09-23 14:59:37 +0500

    Initial Commit

---
+++ Pyramid/PyramidApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyramidApp.swift
+//  Pyramid
+//
+//  Created by Артём Коротков on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PyramidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
